a7f6ffd96b17c618aac9d607be88e0ec6d05444e
Addisu Haile Tedla <addisu.haile@yahoo.com>
2022-09-19 21:01:53 +0300

    Add likes controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,14 @@
 class LikesController < ApplicationController
+  def create
+    @post = Post.find(params[:post_id])
+    @like = @post.likes.create(params[:id])
+    redirect_to post_path(@post)
+  end
+
+  def destroy
+    @post = Post.find(params[:post_id])
+    @like = @post.likes.find(params[:id])
+    @like.destroy
+    redirect_to post_path(@post)
+  end
 end
