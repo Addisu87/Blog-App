5c30df32d628d67d3124320c500b2f06086ba099
Addisu Haile Tedla <addisu.haile@yahoo.com>
2022-09-22 18:36:48 +0300

    Modify seed file

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_14_123949) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_14_123618) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "comments", force: :cascade do |t|
-    t.text "text"
-    t.bigint "author_id"
-    t.bigint "post_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["author_id"], name: "index_comments_on_author_id"
-    t.index ["post_id"], name: "index_comments_on_post_id"
-  end
 
   create_table "likes", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -53,8 +43,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_14_123949) do
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "comments", "posts"
-  add_foreign_key "comments", "users", column: "author_id"
   add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users", column: "author_id"
   add_foreign_key "posts", "users", column: "author_id"
