ee62b66337e9cf63d286bd99cc0217ed722e2106
Addisu Haile Tedla <addisu.haile@yahoo.com>
2022-09-22 18:08:18 +0300

    Modify user migration

--- db/migrate/20220914122650_create_users.rb
+++ db/migrate/20220914122650_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration[7.0]
     create_table :users do |t|
       t.string :name
       t.text :bio
-      t.string :photo
+      t.string :photo, null: false, 'https://api.lorem.space/image/fashion?w=150&h=150&t=1'
       t.integer :posts_counter, null: false, default: 0
 
       t.timestamps
