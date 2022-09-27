53a6d8308e8a98d53008870f1a83f2886865f595
Addisu Haile Tedla <addisu.haile@yahoo.com>
2022-09-27 21:00:57 +0300

    Add additional tests for posts

--- spec/features/posts_show_spec.rb
+++ spec/features/posts_show_spec.rb
@@ -2,31 +2,33 @@ require 'rails_helper'
 
 RSpec.describe 'Show post page: ', type: :feature do
   before(:each) do
-    @user = User.create(name: 'Addisu', photo: 'img_1.png', bio: 'Physics teacher', posts_counter: 0)
-    @user.save!
+    @user_one = User.create(name: 'Addisu', photo: 'img_1.png', bio: 'Physics teacher', posts_counter: 0)
+    @user_one.save!
+    visit root_path
 
-    @post_one = Post.create(title: 'First post', text: 'Hello world!', id: 1, comments_counter: 0, likes_counter: 0)
+    @post_one = Post.create(title: 'First post', text: 'Hello world!', id: 1, comments_counter: 0, likes_counter: 0,
+                            author: @user_one)
+    @post_two = Post.create(title: 'Second post', text: 'Quantum Mechanics!', id: 2, comments_counter: 0,
+                            likes_counter: 0, author: @user_one)
 
-    @comment_one = Comment.create(post: @post, user: @user, text: 'First comment')
-    @comment_one.save!
-    @comment_two = Comment.create(post: @post, user: @user, text: 'Second comment')
-
-    visit "/users/#{@user.id}/posts/#{@post.id}"
+    @comment_one = Comment.create(author: User.first, post: Post.first)
+    @comment_two = Comment.create(author: User.first, post: Post.first)
+    visit user_posts_path(@user_one, @post_one)
   end
 
   it 'shows the name of the author' do
-    expect(page).to have_content(@user.name)
+    expect(page).to have_content(@user_one.name)
   end
 
   it 'shows the posts title' do
-    expect(page).to have_content(@post.title)
+    expect(page).to have_content(@post_two.title)
   end
 
   it 'shows the number of comments for a post' do
-    expect(page).to have_content("Comments: #{@post.comments_counter}")
+    expect(page).to have_content("Comments: #{@post_one.comments_counter}")
   end
 
   it 'shows the number of likes for a post' do
-    expect(page).to have_content("Likes: #{@post.likes_counter}")
+    expect(page).to have_content("Likes: #{@post_two.likes_counter}")
   end
 end
