da88a54b985a8d62db5dd2e3e6ec7662f3e2fa36
Addisu Tedla <addisutedla@Addisus-MacBook-Air.local>
2022-12-02 16:20:19 +0300

    Update user controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,26 +1,23 @@
 class Api::V1::UsersController < ApplicationController
-  before_action :authorize_request, except: %i[index]
-  before_action :find_user, except: %i[index]
+  skip_before_action :authorized, only: [:create]
 
-  # GET /users
-  def index
-    @users = User.all
-
-    respond_to do |format|
-      format.json { render json: @users }
-    end
+  def profile
+    render json: { user: UserSerializer.new(current_user) }, status: :accepted
   end
 
-  # GET /users/:id
-  def show
-    render json: @user, status: :ok
+  def create
+    @user = User.create(user_params)
+    if @user.valid?
+      @token = encode_token(user_id: @user.id)
+      render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
+    else
+      render json: { error: 'failed to create user' }, status: :not_acceptable
+    end
   end
 
   private
 
-  def find_user
-    @user = User.find_by_id!(params[:id])
-  rescue ActiveRecord::RecordNotFound
-    render json: { errors: 'User not found' }, status: :not_found
+  def user_params
+    params.require(:user).permit(:name, :password)
   end
 end
